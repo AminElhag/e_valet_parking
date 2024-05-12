8cc32f6465a6ac6bb49a9f292bfdfdb18acd5bcb
amin_elhag <amingalalzoom@gmail.com>
2024-05-12 13:20:04 +0200

    Remove all init code create a login page

---
+++ e_valet_parking_front_end_app/lib/login/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LgoinPageState();
+}
+
+class _LgoinPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
